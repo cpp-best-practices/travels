69ab429cba208ee6adf53b0be4175b0e5cb0cf1c
Jason Turner <jason@emptycrate.com>
2023-08-27 18:15:09 -0600

    Enable conversation/menus/popups from script

--- src/scripted_game.hpp
+++ src/scripted_game.hpp
@@ -21,6 +21,64 @@ struct Scripted_Game
     game.maps_3d.emplace(std::string(engine.strings.view(name)),
       lefticus::travels::load_3d_map(engine.strings.view(path), m_search_paths));
   }
+  void show_popup(engine_type::string_type name)
+  {
+    game.popup_message = std::string(engine.strings.view(name));
+  }
+
+  void set_menu(engine_type::list_type list)
+  {
+    lefticus::travels::Menu menu;
+    for (const auto &menu_item : engine.values[list]) {
+      if (const auto *item = engine.get_if<engine_type::list_type>(&menu_item); item != nullptr) {
+        if (item->size >= 2) {
+          const auto *text = engine.get_if<engine_type::string_type>(&engine.values[(*item)[0]]);
+          const auto *message = engine.get_if<engine_type::string_type>(&engine.values[(*item)[1]]);
+
+          if (text != nullptr && message != nullptr) {
+
+            std::function<bool(const lefticus::travels::Game &)> test{ [](const auto &) { return true; } };
+            std::function<void(lefticus::travels::Game &)> action{ [msg = std::string{ engine.strings.view(*message) }](
+                                                                     lefticus::travels::Game &game_) {
+              if (!msg.empty()) { game_.popup_message = msg; }
+            } };
+
+            for (const auto &element : engine.values[item->sublist(2)]) {
+              if (const auto *element_list = engine.get_if<engine_type::list_type>(&element); element_list != nullptr) {
+                const auto *identifier =
+                  engine.get_if<engine_type::identifier_type>(&engine.values[(*element_list)[0]]);
+                const auto action_or_test = engine.values[(*element_list)[1]];
+
+                if (identifier != nullptr) {
+                  if (engine.strings.view(identifier->value) == "action") {
+                    action = [this,
+                               action,
+                               callable = engine.make_callable<void()>(
+                                 engine.eval(engine.global_scope, action_or_test))](lefticus::travels::Game &game_) {
+                      action(game_);
+                      callable(engine);
+                    };
+                  } else if (engine.strings.view(identifier->value) == "test") {
+                    test = [this,
+                             test,
+                             callable = engine.make_callable<bool()>(engine.eval(engine.global_scope, action_or_test))](
+                             const lefticus::travels::Game &game_) -> bool {
+                      auto result = callable(engine);
+                      return test(game_) && result.value();
+                    };
+                  }
+                }
+              }
+            }
+            menu.items.emplace_back(std::string(engine.strings.view(*text)), action, test);
+          }
+        }
+      }
+    }
+    menu.items.emplace_back(lefticus::travels::exit_menu());
+
+    game.set_menu(menu);
+  }
 
   void teleport_to_2d(engine_type::string_type map_name, engine_type::string_type location_name)
   {
@@ -33,6 +91,21 @@ struct Scripted_Game
       std::string(engine.strings.view(map_name)), std::string(engine.strings.view(location_name)), angle);
   }
 
+  bool get_boolean(engine_type::string_type bool_name)
+  {
+    auto var = game.variables[std::string(engine.strings.view(bool_name))];
+    if (std::holds_alternative<bool>(var)) {
+      return std::get<bool>(var);
+    } else {
+      return false;
+    }
+  }
+
+  void set_boolean(engine_type::string_type bool_name, const bool value)
+  {
+    game.variables[std::string(engine.strings.view(bool_name))] = value;
+  }
+
   void set_3d_location_action(engine_type::string_type map_name,
     engine_type::string_type location_name,
     engine_type::SExpr callable)
@@ -42,8 +115,7 @@ struct Scripted_Game
 
 
     map.enter_actions.emplace(location_char,
-      [this, callable = engine.make_callable<void()>(callable)](
-        lefticus::travels::Game &, char) { callable(engine); });
+      [this, callable = engine.make_callable<void()>(callable)](lefticus::travels::Game &, char) { callable(engine); });
   }
 
   void set_location_action(engine_type::string_type map_name,
@@ -52,7 +124,7 @@ struct Scripted_Game
   {
     auto &map = game.maps.at(std::string(engine.strings.view(map_name)));
     map.locations.at(map.location_names[std::string(engine.strings.view(location_name))]).enter_action =
-      [this, callable = engine.make_callable<void ()>(callable)](
+      [this, callable = engine.make_callable<void()>(callable)](
         lefticus::travels::Game &, lefticus::travels::Point, lefticus::travels::Direction) { callable(engine); };
   }
 
@@ -75,6 +147,10 @@ struct Scripted_Game
     engine.add<&Scripted_Game::set_3d_location_action>("set_3d_location_action");
     engine.add<&Scripted_Game::teleport_to_3d>("teleport_to_3d");
     engine.add<&Scripted_Game::load_3d_map>("load_3d_map");
+    engine.add<&Scripted_Game::set_menu>("set_menu");
+    engine.add<&Scripted_Game::get_boolean>("get_boolean");
+    engine.add<&Scripted_Game::set_boolean>("set_boolean");
+    engine.add<&Scripted_Game::show_popup>("show_popup");
     return engine;
   }
 
